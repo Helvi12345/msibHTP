5809e9bd2036f21ad11e059fb417101978670504
Helvi12345 <helviayu863@gmail.com>
2023-03-15 21:11:58 +0700

    Upload tugas JS perhitungan menggunakan JS dan CSS

--- js/kalkulator.js
+++ js/kalkulator.js
@@ -3,7 +3,11 @@ function tambah() {
     var a1 = parseFloat(frm.angka.value);
     var a2 = parseFloat(frm.angka2.value);
     var total = a1 + a2;
-    frm.hasil.value = total;
+    if (frm.angka.value == "" || frm.angka2.value == "") {
+        alert("Maaf angka belum terisi");
+    } else {
+        frm.hasil.value = total;
+    }
 }
 
 function kurang() {
@@ -11,5 +15,45 @@ function kurang() {
     var a1 = parseFloat(frm.angka.value);
     var a2 = parseFloat(frm.angka2.value);
     var total = a1 - a2;
-    frm.hasil.value = total;
+    if (frm.angka.value == "" || frm.angka2.value == "") {
+        alert("Maaf angka belum terisi");
+    } else {
+        frm.hasil.value = total;
+    }
+
+}
+function kali() {
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 * a2;
+    if (frm.angka.value == "" || frm.angka2.value == "") {
+        alert("Maaf angka belum terisi");
+    } else {
+        frm.hasil.value = total;
+    }
+}
+
+function bagi() {
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 / a2;
+    if (frm.angka.value == "" || frm.angka2.value == "") {
+        alert("Maaf angka belum terisi");
+    } else {
+        frm.hasil.value = total;
+    }
+}
+
+function pangkat() {
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 ** a2;
+    if (frm.angka.value == "" || frm.angka2.value == "") {
+        alert("Maaf angka belum terisi");
+    } else {
+        frm.hasil.value = total;
+    }
 }
