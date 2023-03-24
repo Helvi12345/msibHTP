cc1e8a9a6a3b22a9a82ae113cc190ca316409231
Helvi12345 <helviayu863@gmail.com>
2023-03-24 10:56:56 +0700

    File baru php

---
+++ file8.php
@@ -0,0 +1,19 @@
+<form method="POST">
+    <label>Username</label><br>
+    <input type="text" name="uname"><br>
+    <label>password</label><br>
+    <input type="password" name="pass"><br>
+
+    <input type="submit" name="login" value="login"><br>
+</form>
+
+<?php
+error_reporting(0);
+//
+$username = $_POST['uname'];
+$password = $_POST['pass'];
+$login = $_POST['login'];
+if (isset($login)) {
+    echo 'Username ' . $username . '<br> password :' . $password;
+}
+?>
