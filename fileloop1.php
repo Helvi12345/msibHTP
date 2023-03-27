dccbbd29bd68610ff66476c0ae9a36ad33fe710b
Helvi12345 <helviayu863@gmail.com>
2023-03-27 10:53:06 +0700

    file baru looping

---
+++ fileloop1.php
@@ -0,0 +1,34 @@
+<?php
+//jika tidak menggunakan looping
+echo 'Bilangan 1';
+echo '<br> Bilangan 2';
+echo '<hr>';
+//ini contoh looping increment
+for ($x = 1; $x <= 10; $x++) {
+    echo '<br> Bilangan ' . $x;
+}
+//ini contoh looping decrement
+for ($y = 10; $y >= 1; $y--) {
+    echo '<br> Angka ' . $y;
+}
+///looping menggunakan while
+echo '<hr> looping menggunakan while';
+echo '<hr>';
+
+$xz = 1;
+while ($xz <= 5) {
+    echo '<br> Bilangan' . $xz;
+    $xz++;
+}
+$yz = 5;
+while ($yz >= 1) {
+    echo '<br> Bilangan' . $yz;
+    $yz--;
+}
+$d = 1;
+do {
+    echo '<br>' . $d;
+    $d++;
+} while ($d <= 10);
+?>
+.
